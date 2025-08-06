8489f86d8b86a4654cb136f9df10a688b8ade1e9
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-08-06 18:55:55 -0300

    Update ec2ebs2.0.sh

--- ec2ebs2.0.sh
+++ ec2ebs2.0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 STANCE_TYPE="t3.micro"
-AVAILABITY_ZONE="sa-east=1"
+AVAILABITY_ZONE="sa-east-1"
 create_EC2 () {
 
 AMI="ami-020cba7c55df1f615"
@@ -11,15 +11,16 @@ KEY_NAME="girl"
 
 Volumesize=8
 
-INSTANCE_ID=$(aws ec2 run-instances \ 
-
---image-id ""$AMI"" \
---availabity_zone "$AVAILABITY_ZONE"
---instance-ids "$STANCE_TYPE" \ 
+INSTANCE_ID=$(aws ec2 run-instances \
+--image-id "$AMI" \
+--instance-type "$STANCE_TYPE" \
+--placement AvailabilityZone="$AVAILABITY_ZONE" \
 --key-name "$KEY_NAME" \
 --security-groups "$Firewall" \
---block-device-mappings "{"DeviceName":"/dev/sdf","Ebs"\":{\"Volumeid\":$Volumeid}}]"
-aws ec2 wait ec2-in-use --ec2-ids "$AMI"
+--block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":$Volumesize}}]" \
+--query "Instances[0].InstanceId" \
+--output text)
+aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
 }
 
 create_EBS () {
@@ -28,17 +29,22 @@ SIZE=8
 IOPS=3000
 Attached="/dev/sdf"
 VOLUME_TYPE="io1"
-Volumesize=$(aws ec2 create-volume \
---size_gb="$SIZE_GB"
---availabity_zone "$AVAILABITY_ZONE"
- --iops "$IOPS" \
- --instance-id "$INSTANCE_ID" \
- --device "$Attached"
- aws ec2 wait volume-in-use --volume-ids "$volume_ID"
- sleep 10
+volume_ID=$(aws ec2 create-volume \
+--size "$SIZE" \
+--availability-zone "$AVAILABITY_ZONE" \
+--iops "$IOPS" \
+--volume-type "$VOLUME_TYPE" \
+--query "VolumeId" \
+--output text)
+aws ec2 wait volume-available --volume-ids "$volume_ID"
+aws ec2 attach-volume \
+--volume-id "$volume_ID" \
+--instance-id "$INSTANCE_ID" \
+--device "$Attached"
+aws ec2 wait volume-in-use --volume-ids "$volume_ID"
+sleep 10
 
 }
-
 
 mount_ebs () {
 DEVICE_NAME="/dev/sdf"
@@ -47,7 +53,7 @@ sudo mkfs -t ext4 $DEVICE_NAME
 sudo mkdir -p $MOUNT_POINT
 sudo mount $DEVICE_NAME $MOUNT_POINT
 
-echo " $DEVICE_NAME $MOUNT_POINT ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+echo "$DEVICE_NAME $MOUNT_POINT ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
 }
 
 create_EC2
